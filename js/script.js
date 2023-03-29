b2fd4202592369e340ed74d4ca9c05e14bee5c12
mirkettinho <mirkodifranco30@gmail.com>
2023-03-29 17:25:33 +0200

    [add] toggle click

--- js/script.js
+++ js/script.js
@@ -2,15 +2,23 @@ const container = document.querySelector(".container")
 
 const start = document.querySelector(".start-match")
 
+start.off = true;
 console.log(container, start)
 
-
+// BOTTONE
 start.addEventListener("click" ,function(){
-
-  for(let i = 0; i < 100; i++){
+  
+  for(let i = 1; i <= 100; i++){
     const quadrato = generateSquare();
     console.log(quadrato)
     container.append(quadrato)
+    quadrato.Id = i;
+
+    quadrato.addEventListener("click", function(){
+      this.classList.toggle("click")
+      console.log(this.Id)
+      
+    })
   }
   
 

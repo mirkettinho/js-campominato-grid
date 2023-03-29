03dbc8accbc9c65cf303894cc14189917078cca4
mirkettinho <mirkodifranco30@gmail.com>
2023-03-29 22:23:48 +0200

    [fix] toggle click

--- js/script.js
+++ js/script.js
@@ -2,17 +2,29 @@ const container = document.querySelector(".container")
 
 const start = document.querySelector(".start-match")
 
-start.off = true;
+const difficolta = document.getElementById("difficolta")
+
+
 console.log(container, start)
 
 // BOTTONE
 start.addEventListener("click" ,function(){
   
-  for(let i = 1; i <= 100; i++){
+
+  for(let i = 1; i <= difficolta.value; i++){
     const quadrato = generateSquare();
     console.log(quadrato)
     container.append(quadrato)
     quadrato.Id = i;
+
+
+    if(difficolta.value == 81){
+      quadrato.classList.add("square-nine")
+    }else if(difficolta.value == 49){
+      quadrato.classList.add("square-seven")
+    }else{
+      quadrato.classList.add("square-ten")
+    }
 
     quadrato.addEventListener("click", function(){
       this.classList.toggle("click")
@@ -20,14 +32,16 @@ start.addEventListener("click" ,function(){
       
     })
   }
-  
 
+    
+  
 
   function generateSquare(){
     const generareQuadrato = document.createElement("div");
     generareQuadrato.className = "square";
     return generareQuadrato;
   }
+
 })
 
 
